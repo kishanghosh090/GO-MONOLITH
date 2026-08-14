e913e87092e8e3fe366b9981771b9a0b62f2108a
kishanghosh090 <kishanghosh090@gmail.com>
2026-08-14 20:10:49 +0530

    add configs

--- olx-api/cmd/api/main.go
+++ olx-api/cmd/api/main.go
@@ -4,25 +4,30 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
 	"time"
+
+	"github.com/kishanghosh090/GO-MONOLITH/internal/config"
 )
 
 func main() {
+	config.MustLoad()
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"msg":"chai aur code"}`))
+		w.Write([]byte(`{"msg":"server is running fine"}`))
 	})
 
 	srv := http.Server{
-		Addr:         ":8090",
+		Addr:         ":" + os.Getenv("PORT"),
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
+	log.Printf("Server is listing on %s", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
--- olx-api/go.mod
+++ olx-api/go.mod
@@ -1,3 +1,5 @@
 module github.com/kishanghosh090/GO-MONOLITH
 
 go 1.26.5
+
+require github.com/joho/godotenv v1.5.1
